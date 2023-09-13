79e37edc7fc9042edf95ef93b8c58d8915af213f
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-13 14:55:31 -0500

    terminar initView y llamarlo en onCreate

--- AppSemana5Sesion1/app/src/main/java/com/example/appsemana5sesion1/MainActivity.kt
+++ AppSemana5Sesion1/app/src/main/java/com/example/appsemana5sesion1/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.appsemana5sesion1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +16,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         loadContacts()
+
+        initView()
     }
 
     private fun initView(){
         val rvContact = findViewById<RecyclerView>(R.id.rvContact)
+
+        rvContact.adapter = contactAdapter
+        rvContact.layoutManager = LinearLayoutManager(this)
     }
 
     private fun loadContacts() {
