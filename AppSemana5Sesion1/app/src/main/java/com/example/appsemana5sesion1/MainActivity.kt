31c1ee8855d38d709837fa969cdb1528bf7c19a7
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-13 14:34:21 -0500

    declarar el Adapter

--- AppSemana5Sesion1/app/src/main/java/com/example/appsemana5sesion1/MainActivity.kt
+++ AppSemana5Sesion1/app/src/main/java/com/example/appsemana5sesion1/MainActivity.kt
@@ -7,6 +7,8 @@ class MainActivity : AppCompatActivity() {
 
     var contacts = ArrayList<Contact>()
 
+    var contactAdapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
