c1d533f80c9652936c2824c32a8882efad56db1b
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-13 14:41:07 -0500

    colocar en la clase el arreglo y colcar el getItemCount

--- AppSemana5Sesion1/app/src/main/java/com/example/appsemana5sesion1/ContactAdapter.kt
+++ AppSemana5Sesion1/app/src/main/java/com/example/appsemana5sesion1/ContactAdapter.kt
@@ -1,7 +1,19 @@
 package com.example.appsemana5sesion1
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 
-class ContactAdapter: Adapter<ContactPrototype>() {
+class ContactAdapter(var contacts: ArrayList<Contact>): Adapter<ContactPrototype>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContactPrototype {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ContactPrototype, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return contacts.size
+    }
 
 }
