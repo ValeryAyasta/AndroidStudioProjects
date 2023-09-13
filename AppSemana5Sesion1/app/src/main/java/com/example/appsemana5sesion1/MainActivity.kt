c048d25a132b410facd8dffef986558fc5e8c5c2
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-13 14:30:45 -0500

    CREAR METODO para cargar los contactos (clase para los elementos) y llamar en OnCreate

--- AppSemana5Sesion1/app/src/main/java/com/example/appsemana5sesion1/MainActivity.kt
+++ AppSemana5Sesion1/app/src/main/java/com/example/appsemana5sesion1/MainActivity.kt
@@ -10,5 +10,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadContacts()
     }
+
+    private fun loadContacts() {
+        contacts.add(Contact("Lucho Lopez", "123456789"))
+        contacts.add(Contact("Lucia Lopez", "123400789"))
+        contacts.add(Contact("Luz Lopez", "123456789"))
+        contacts.add(Contact("Lucas Lopez", "123456700"))
+        contacts.add(Contact("Luna Lopez", "120056789"))
+    }
+
+
 }
