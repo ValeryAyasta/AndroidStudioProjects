7366b1f63dc0ca536a678d81b5ff0f9ee6519eb5
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-15 11:26:25 -0500

    crear metodo load

--- Semana5Sesion2Networking/app/src/main/java/com/example/semana5sesion2networking/MainActivity.kt
+++ Semana5Sesion2Networking/app/src/main/java/com/example/semana5sesion2networking/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.semana5sesion2networking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btJoke = findViewById<Button>(R.id.btJoke)
+
+        btJoke.setOnClickListener {
+            loadJoke()
+        }
+
+    }
+
+    private fun loadJoke() {
+        //codigo para que se muestre la información del servicio de API
+       //en el textView
+
+        //https://geek-jokes.sameerkumar.website/api?format=json
+
+        //poner el codigo de retrofit
+
+
+
     }
 }
