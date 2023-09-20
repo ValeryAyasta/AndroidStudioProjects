51ba93d2a16139f3d51153e95bfbe54d0c526f09
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-20 14:58:48 -0500

    heredar la abstract class y colocar anotacion @Database

--- Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/database/AppDatabase.kt
+++ Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/database/AppDatabase.kt
@@ -1,4 +1,11 @@
 package com.example.semana6sesion1.database
 
-class AppDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.semana6sesion1.models.Contact
+
+@Database(entities = arrayOf(Contact::class), version = 1)
+abstract class AppDatabase : RoomDatabase()
+{
+
 }
