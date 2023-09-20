3e84704cd08108580e48dbb200c1e439f85aa390
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-20 15:28:47 -0500

    load contacts

--- Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/MainActivity.kt
+++ Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.semana6sesion1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.semana6sesion1.database.AppDatabase
+import com.example.semana6sesion1.models.Contact
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var contacts : List<Contact>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        loadContacts()
+    }
+
+    private fun loadContacts() {
+       contacts = AppDatabase.getInstance(this).getDao().getAll()
     }
 }
