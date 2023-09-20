cd37d4422716021bce4d525aa4717c0ad5f884ac
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-20 14:43:34 -0500

    colocar las anotaciones en los atributos

--- Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/models/Contact.kt
+++ Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/models/Contact.kt
@@ -1,10 +1,18 @@
 package com.example.semana6sesion1.models
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 @Entity
 class Contact (
+
+    @PrimaryKey(autoGenerate = true)
     val id: Int,
+
+    @ColumnInfo //( name = "apellido-nombre")
     val name:String,
+
+    @ColumnInfo
     val telephone:String
 )
