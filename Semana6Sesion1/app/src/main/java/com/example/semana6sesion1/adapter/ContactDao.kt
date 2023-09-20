0a18f9f6a5573898de685fb2ab7298e67236a079
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-20 14:55:38 -0500

    colocar las annotations en los methods de dao

--- Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/adapter/ContactDao.kt
+++ Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/adapter/ContactDao.kt
@@ -1,4 +1,25 @@
 package com.example.semana6sesion1.adapter
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.semana6sesion1.models.Contact
+
+@Dao
 interface ContactDao {
+    //Room trabaja con List, no con ArrayList
+
+    @Query("SELECT * FROM Contact")
+    fun getAll(): List<Contact>
+
+    @Insert
+    fun insertContact(vararg contacts: Contact)
+
+    @Delete
+    fun deleteContact(vararg contacts: Contact)
+
+    @Update
+    fun updateContact(vararg contacts: Contact)
 }
