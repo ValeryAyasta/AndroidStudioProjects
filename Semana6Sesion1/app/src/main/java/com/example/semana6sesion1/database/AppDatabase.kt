b505788500d04d6ab0a11a887c58f5b5f2053ec4
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-20 15:03:12 -0500

    crear abstract fun y companion object

--- Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/database/AppDatabase.kt
+++ Semana6Sesion1/app/src/main/java/com/example/semana6sesion1/database/AppDatabase.kt
@@ -1,11 +1,29 @@
 package com.example.semana6sesion1.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.semana6sesion1.adapter.ContactDao
 import com.example.semana6sesion1.models.Contact
 
 @Database(entities = arrayOf(Contact::class), version = 1)
 abstract class AppDatabase : RoomDatabase()
 {
+    //usamos el patron "singleton"
 
+    abstract fun getDao() : ContactDao
+    companion object{
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context : Context) : AppDatabase{
+            if (INSTANCE == null){
+                INSTANCE = Room
+                    .databaseBuilder(context, AppDatabase::class.java, "mycontact.db")
+                    .allowMainThreadQueries()
+                    .build()
+            }
+            return INSTANCE as AppDatabase
+        }
+    }
 }
