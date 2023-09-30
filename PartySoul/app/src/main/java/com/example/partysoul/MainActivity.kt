f03ffec877c1dd3e6e8c3688c695548d16ad6028
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-30 12:28:45 -0500

    completar el request en load

--- PartySoul/app/src/main/java/com/example/partysoul/MainActivity.kt
+++ PartySoul/app/src/main/java/com/example/partysoul/MainActivity.kt
@@ -1,8 +1,17 @@
 package com.example.partysoul
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.partysoul.adapter.JokeAdapter
+import com.example.partysoul.models.ApiResponseHeader
+import com.example.partysoul.models.Joke
+import com.example.partysoul.network.JokeService
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -14,11 +23,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        loadJokes()
+        loadJokes(this)
     }
 
 
-    private fun loadJokes() {
+    private fun loadJokes(context : Context) {
         val rvJokes = findViewById<RecyclerView>(R.id.rvJokes)
 
         //lamar a retrofit
@@ -29,5 +38,30 @@ class MainActivity : AppCompatActivity() {
             .build()
 
         val jokeService : JokeService
+
+        jokeService = retrofit.create(JokeService::class.java)
+
+        val request = jokeService.getJokes("application/json")
+
+        request.enqueue(object : Callback<ApiResponseHeader>{
+            override fun onResponse(
+                call: Call<ApiResponseHeader>,
+                response: Response<ApiResponseHeader>
+            ) {
+                if (response.isSuccessful){
+                    val jokes: List<Joke> = response.body()!!.results?: ArrayList()
+
+                    rvJokes.layoutManager = LinearLayoutManager(this@MainActivity)
+                    rvJokes.adapter = JokeAdapter(jokes, context)
+
+                }
+            }
+
+            override fun onFailure(call: Call<ApiResponseHeader>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
     }
 }
