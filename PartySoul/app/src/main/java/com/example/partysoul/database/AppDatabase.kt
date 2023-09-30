f81c407bc21f2b6c673c567b110f85543badc54b
Valery Ruth Ayasta Leonardo <102206366+ValeryAyasta@users.noreply.github.com>
2023-09-30 14:02:16 -0500

    crear clase de la base de datos

---
+++ PartySoul/app/src/main/java/com/example/partysoul/database/AppDatabase.kt
@@ -0,0 +1,26 @@
+package com.example.partysoul.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.partysoul.models.Joke
+
+@Database(entities = arrayOf(Joke::class), version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun getDao() : JokeDao
+    companion object{
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context : Context) : AppDatabase{
+            if (INSTANCE == null){
+                INSTANCE = Room
+                    .databaseBuilder(context, AppDatabase::class.java, "party-soul.db")
+                    .allowMainThreadQueries()
+                    .build()
+            }
+            return INSTANCE as AppDatabase
+        }
+    }
+
+}
